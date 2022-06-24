71dc824b2e9058c8445fe564383223a77ab29388
Rinky <rinkymaurya06767@gmail.com>
2022-06-24 21:00:42 +0530

    List Event

--- src/Component/DropdownCategory.js
+++ src/Component/DropdownCategory.js
@@ -3,22 +3,20 @@ import { useState} from 'react';
 
 function DropdownCategory(prop) {
    let [show, setShow] = useState("None");
-   let selected = "None";
 
    function clickHandler(event) {
-      if (event.target.value !== "None" && event.target.value !== selected) {
-         selected = event.target.value;
+         
          setShow(event.target.value);
          prop.fun(show);
-      }
    }
+   console.log("show: "+show);
    return (<>
       <div className='Listconatiner1'>
          <label id="head">Select a Category</label>
-         <select id="drop" onChange={clickHandler} value={show} ><option value="None" >None</option>
+         <select id="drop" onChange={clickHandler} value={show} onClick={clickHandler}><option value="None" >None</option>
             {
                prop.cat.map((val, index) => {
-                  return <option key={index} value={val.strCategory} className="options">{val.strCategory}</option>
+                  return <option key={index} value={val.strCategory} className="options" onClick={clickHandler} onTouchEnd={clickHandler}>{val.strCategory}</option>
                })
             }
          </select>
--- src/Component/ItemCard.js
+++ src/Component/ItemCard.js
@@ -1,19 +1,21 @@
 import './item.css'
 import { Link } from 'react-router-dom';
+import PageNotFound from "./Error/PageNotFoound";
 
 function ItemCard(prop){
     return(
     <ul className="wrapperitems">
     {
-        prop.list.map((val)=>{
-          return( <>
-          <li className="itemcontainer" key={val.idMeal}>
-            <h1 className="headingmain">{val.strMeal}</h1>
-            <img src={val.strMealThumb} className="picmain" alt="yummy"/>
-            <button data-id={val.idMeal}className="ingbtn" ><Link style={{color:"white",textDecoration:"none"}} to={`/recipe/${val.idMeal}`}>Ingredient</Link></button>
-            </li>
-            </>) 
-        })
+       prop.list!==null?( prop.list.map((val)=>{
+        return( <>
+        <li className="itemcontainer" key={val.idMeal}>
+          <h1 className="headingmain">{val.strMeal}</h1>
+          <img src={val.strMealThumb} className="picmain" alt="yummy"/>
+          <button data-id={val.idMeal}className="ingbtn" ><Link style={{color:"white",textDecoration:"none"}} to={`/recipe/${val.idMeal}`}>Ingredient</Link></button>
+          </li>
+          </>) 
+      })):(<PageNotFound/>)
+       
     }
     </ul>)
 }
