2777d43f090b23722525a0057a791cdcce07331b
Rinky <rinkymaurya06767@gmail.com>
2022-06-24 20:27:50 +0530

    Changes

--- src/Component/DropdownCategory.js
+++ src/Component/DropdownCategory.js
@@ -15,11 +15,7 @@ function DropdownCategory(prop) {
    return (<>
       <div className='Listconatiner1'>
          <label id="head">Select a Category</label>
-<<<<<<< HEAD
          <select id="drop" onChange={clickHandler} value={show} ><option value="None" >None</option>
-=======
-         <select id="drop" onChange={clickHandler} onClick={clickHandler} ><option value="None" >None</option>
->>>>>>> 85e3492049887236a969aeabceac50f5047a3154
             {
                prop.cat.map((val, index) => {
                   return <option key={index} value={val.strCategory} className="options">{val.strCategory}</option>
