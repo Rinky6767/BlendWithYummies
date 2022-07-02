3bbcd8bed9c213dcdba1b15dbcf7e28f5746c15a
Rinky <71222541+Rinky6767@users.noreply.github.com>
2022-07-02 19:04:52 +0530

    Update ItemsCards.js

--- src/Component/ItemsCards.js
+++ src/Component/ItemsCards.js
@@ -1,30 +1,18 @@
-import { useContext, useEffect, useState } from 'react';
-import {FaRegFrown} from 'react-icons/fa';
+import { useEffect, useState } from 'react';
+import { FaRegFrown } from 'react-icons/fa';
 import './itemcards.css';
 import ItemCard from './ItemCard';
-import {NameContext} from "./Context/Name";
 
 function ItemCards(prop) {
-let nameitem= useContext(NameContext);
-    let proName= prop.name;
+    let proName = prop.name;
     let [parC, setPar] = useState("None");
-    let prevdata= useContext(NameContext);
-    if(prevdata!==null){prevdata=[]}
-    let [data,setData]=useState(prevdata);
-    if (proName !== "None" && proName !== parC) {
+    let [data, setData] = useState([]);
+    if ( proName !== parC) {
         setPar(proName);
     }
-
-  else if(proName==="None")
-   {
-   
-   if(nameitem.length>0){
-   setPar(nameitem);
-   }
- }
     useEffect(() => {
-        if (proName !== "None") {
-            let url= `https://www.themealdb.com/api/json/v1/1/filter.php?c=${parC}`;
+        if (parC !== "None") {
+            let url = `https://www.themealdb.com/api/json/v1/1/filter.php?c=${parC}`;
             let catC = fetch(url);
             catC.then((response) => {
                 return response.json();
@@ -32,18 +20,16 @@ let nameitem= useContext(NameContext);
                 setData(data.meals);
             })
         }
-       // eslint-disable-next-line 
+        // eslint-disable-next-line 
     }, [parC]);
 
     return (
-      <NameContext.Provider value={parC}>
-      <div id="ItemCardsWrapper">
-        {
-            parC === "None" ? (<><h1 className='nodis'>Nothing to Display<FaRegFrown className='face' style={{color:"blue"}}/></h1></>) : (<ItemCard list={data}/>)
-        }
-    </div>
-    </NameContext.Provider>
-     )
+            <div id="ItemCardsWrapper">
+                {
+                    parC === "None" ? (<><h1 className='nodis'>Nothing to Display<FaRegFrown className='face' style={{ color: "blue" }} /></h1></>) : (<ItemCard list={data} />)
+                }
+            </div>
+    )
 
 }
 export default ItemCards;
