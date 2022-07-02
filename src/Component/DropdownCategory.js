65e9552755bee773ebb8aefe56c5c14868cbd5c8
Rinky <71222541+Rinky6767@users.noreply.github.com>
2022-07-02 19:06:20 +0530

    Update DropdownCategory.js

--- src/Component/DropdownCategory.js
+++ src/Component/DropdownCategory.js
@@ -1,21 +1,31 @@
 import './DropDownCat.css';
-
+import {useRef} from "react"
 function DropdownCategory(prop) {
+   let refval= useRef();
+   let i= sessionStorage.getItem("catname");
+   // if(refval.current.value!==i){
+   //    refval.current.value=i;
+   // }
    function clickHandler(event) {
-         prop.fun(event.target.value);
+    
+      if (i!==undefined || i!==null){
+         sessionStorage.removeItem("catname");
+      }
+      sessionStorage.setItem("catname",event.target.value);
+      prop.fun(event.target.value);
    }
    return (<>
-      <div className='Listconatiner1'>
-         <label id="head">Select a Category</label>
-         <select id="drop" onClick={clickHandler} onChange={clickHandler} ><option value="None" >None</option>
-            {
-               prop.cat.map((val, index) => {
-                  return <option key={index} value={val.strCategory} className="options" >{val.strCategory}</option>
-               })
-            }
-         </select>
-      </div>
-      </>
+         <div className='Listconatiner1'>
+            <label id="head">Select a Category</label>
+            <select id="drop"  onChange={clickHandler} ref={refval}><option value="None" >None</option>
+               {
+                  prop.cat.map((val, index) => {
+                     return <option key={index} value={val.strCategory} className="options" >{val.strCategory}</option>
+                  })
+               }
+            </select>
+         </div>
+   </>
    );
 }
 
