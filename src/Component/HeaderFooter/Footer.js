8690af12302753a364a48e290d9480b13958883f
Rinky <rinkymaurya06767@gmail.com>
2022-08-11 20:56:34 +0530

    footer size

--- src/Component/HeaderFooter/Footer.js
+++ src/Component/HeaderFooter/Footer.js
@@ -2,7 +2,7 @@ import {FaCoffee,FaHeart} from 'react-icons/fa';
 import './footer.css'
 function Footer(){
 return(<div className="footWrapper">
-<h3>Made Possible with <FaCoffee/> and <FaHeart/> </h3>
+<h3 className="info">Made Possible with <FaCoffee/> and <FaHeart /> </h3>
 </div>)
 }
 export default Footer;
