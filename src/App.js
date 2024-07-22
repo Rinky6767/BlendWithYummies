0e7916ced7c7e1ffdff534c5e95f3462844d4f23
MAURYA Rinky indrajeet <rinky-indrajeet.maurya@alstomgroup.com>
2024-07-22 17:11:12 +0530

    recipe background img change

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {Route, Routes}from 'react-router-dom'
 import Home from './Home'
 import Recipe from './Component/Recipe';
 import Dish from './Dish';
-
 function App() {
   return (
     <div className="App">
